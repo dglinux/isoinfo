6562e810fec81eb6f1a7fd3e87629b388a9812eb
Chuang Zhu <genelocated@yandex.com>
2021-08-03 01:21:34 +0800

    + ubuntu-releases.bash

---
+++ ubuntu-releases.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+fsprefix=${TUNASYNC_WORKING_DIR%/ubuntu-releases}
+
+shopt -s extglob
+
+get_version() {
+    local IFS="-" splitted=()
+    read -ra splitted <<< "$1"
+    echo "${splitted[1]}"
+}
+
+fileobjs=()
+for file in "$TUNASYNC_WORKING_DIR/.pool/"*.iso; do
+    base="$(basename "$file")"
+    version="`get_version "$base"`"
+    url="${file#"$fsprefix"}"
+    fileobj="`jq -nc '{"ver":$ver,"base":$base,"url":$url}'\
+        --arg ver "$version"\
+        --arg base "$base"\
+        --arg url "$url"`"
+    fileobjs=("${fileobjs[@]}" "$fileobj")
+done
+
+jq -nc '[{"name":"NixOS",files:$ARGS.positional}]' --jsonargs "${fileobjs[@]}"
