aae3fba06bcce1f0e6c2c16fc354f5bbee14197a
Chuang Zhu <genelocated@yandex.com>
2021-08-03 16:41:16 +0800

    archlinux.bash + latest + md5, sha1

--- distro/archlinux.bash
+++ distro/archlinux.bash
@@ -3,22 +3,28 @@ fsprefix=${TUNASYNC_WORKING_DIR%/archlinux}
 fileobjs=()
 
 listfiles() {
-    local d
-    for d in "$TUNASYNC_WORKING_DIR/iso/"*; do
-        local base="`basename "$d"`"
-        if [[ -d "$d" && "$base" =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
-            local isos=("$d/"*.iso)
+    local latest="`readlink "$TUNASYNC_WORKING_DIR/iso/latest"`"
+    local version
+    for version in "$TUNASYNC_WORKING_DIR/iso/"*; do
+        local ver="`basename "$version"`"
+        if [[ -d "$version" && "$ver" =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+            local isos=("$version/"*.iso)
             for iso in "${isos[@]}"; do
                 if [[ -f "$iso" ]]; then
                     local url=${iso#"$fsprefix"}
-                    local basefile="`basename "$iso"`"
-                    fileobjs=(
-                        "${fileobjs[@]}"
-                        "`jq -nc '{"ver":$ver,"base":$base,"url":$url}'\
-                            --arg ver "$base"\
-                            --arg url "$url"\
-                            --arg base "$basefile"`"
-                    )
+                    local base="`basename "$iso"`"
+                    local sha1="`grep '\s'"$base"'$' "$version/sha1sums.txt" | cut -d' ' -f 1`"
+                    local md5="`grep '\s'"$base"'$' "$version/md5sums.txt" | cut -d' ' -f 1`"
+                    local fileobj="`jq -nc '{"ver":$ver,"base":$base,"url":$url,"sha1":$sha1,"md5":$md5}'\
+                        --arg ver "$ver"\
+                        --arg url "$url"\
+                        --arg base "$base"\
+                        --arg sha1 "$sha1"\
+                        --arg md5 "$md5"`"
+                    if [[ "$latest" == "$ver" ]]; then
+                        fileobj="`jq -nc '$orig + {"tag":"latest"}' --argjson orig "$fileobj"`"
+                    fi
+                    fileobjs=("${fileobjs[@]}" "$fileobj")
                 fi
             done
         fi
