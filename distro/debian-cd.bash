e729fab8902fb952ca38a7eba883ec3e5e452a60
Chuang Zhu <genelocated@yandex.com>
2021-08-16 09:08:47 +0800

    remove md5 & sha1 checksums for debian-cd
    debian-cd bullseye do not include these any more, they are useless now

--- distro/debian-cd.bash
+++ distro/debian-cd.bash
@@ -12,8 +12,6 @@ get_fileobj() {
     local dir="$(dirname "$file")"
     local base="$(basename "$file")"
     local size="`du -h "$file" | cut -f 1`"
-    local md5="`grep '\s'"$base"'$' "$dir/MD5SUMS" | cut -d' ' -f 1`"
-    local sha1="`grep '\s'"$base"'$' "$dir/SHA1SUMS" | cut -d' ' -f 1`"
     local sha256="`grep '\s'"$base"'$' "$dir/SHA256SUMS" | cut -d' ' -f 1`"
     local sha512="`grep '\s'"$base"'$' "$dir/SHA512SUMS" | cut -d' ' -f 1`"
     jq -nc '{"ver":$ver,"base":$base,"url":$url,"size":$size,"md5":$md5,"sha1":$sha1,"sha256":$sha256,"sha512":$sha512}'\
@@ -21,8 +19,6 @@ get_fileobj() {
         --arg base "$base"\
         --arg url "${url}"\
         --arg size "$size"\
-        --arg md5 "$md5"\
-        --arg sha1 "$sha1"\
         --arg sha256 "$sha256"\
         --arg sha512 "$sha512"
 }
